9a3fda9f81c80168367ef36e5521e16e98635cb1
Einar Jonsson <einar@codelette.com>
2014-02-16 21:04:12 +0000

    Inital app with draper gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DraperDemo::Application.config.secret_key_base = '3be67c5b4fa08246b236799e5782dfac58e75eb10eebd7cc101f4c19592cd21d38faff4363a6d0a7d61eee24b76fc7e134bcfe83923dfcbb727cd8da60cc5782'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DraperDemo::Application.config.session_store :cookie_store, key: '_draper_demo_session'
